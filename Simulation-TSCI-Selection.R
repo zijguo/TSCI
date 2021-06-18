44ed5283c0fb1eef1b0920ca5daf532bde2607ed
ywwes26 <49973874+ywwes26@users.noreply.github.com>
2021-06-18 10:25:32 -0400

    Update Simulation-TSCI-Selection.R

--- Simulation-TSCI-Selection.R
+++ Simulation-TSCI-Selection.R
@@ -1,8 +1,9 @@
-### Start Date: Mar 6, 2021; Recently updated Date: April 30, 2021
+### Start Date: Mar 6, 2021; Recently updated Date: June 18, 2021
 ### TSCI: TSCI using Basis Approach and Random Forest
 
 
 library(MASS)
+library(AER)
 source('Source-Random-Forest.R', encoding = 'UTF-8')
 source('Source-Basis-Approach.R', encoding = "UTF-8")
 
@@ -19,63 +20,54 @@ A1gen<-function(rho,p){
 }
 
 
-###### dimension change the dimension 5,10,20
+###### dimension change the dimension 10,20
 p = 10
 ####please change this n = 1000, 3000, 5000
-n = 5000
-### setting, change across 1, 2, 3, 4, 5, 6, 7
-f.index = 7
-##### change the interaction 0.5, 1, 1.5
-inter.val = 1
+n = 1000
+### setting, change across 1(for previous 1 and 4), 2(for previous 3 and 6)
+f.index = 2
+##### change the interaction 0, 0.5, 1, 1.5
+inter.val = 0.5
 #### a denotes the IV strength, set as 1
 a = 1
 #### violation index, set as 1 or 2
-vio.index = 1
+vio.index = 2
 ##### tau denotes the violation strength
 ##### set tau as 1
 tau = 1
 #### the number of simulation numbers
 nsim = 10
 
-### save difference testing
-H <- beta.diff <- rep(list(NA),nsim)
-z.alpha <- rep(NA,nsim)
 
 ##############################
 f_1 <- function(x){x+a*(x^2+0.5*x^4) -25/12}
-f_2 <- function(x){exp(2*a+x+0.5*x^3) - 2/5 * sinh(5/2)}
-f_3 <- function(x){a*(1*sin(2*pi*x) + 1.5*cos(2*pi*x))}
-f_4 <- function(x){exp(2*a+x+0.5*x^3)+a*(x^2+0.5*x^4)}
+# f_2 <- function(x){exp(2*a+x+0.5*x^3)+a*(x^2+0.5*x^4)}
+f_2 <- function(x){a*(1*sin(2*pi*x) + 1.5*cos(2*pi*x))}
 rho1=0.5
 Cov<-(A1gen(rho1,p+1))
 mu<-rep(0,p+1)
 beta=1
 alpha<-as.matrix(rep(-0.3,p))
 gamma<-as.matrix(rep(0.2,p))
-### assign different structure of interaction as p changes
-if (p>5) {
-  inter<-as.matrix(c(rep(inter.val,5),rep(0,p-5)))
-} else {
-  inter<-as.matrix(rep(inter.val,p))
-}
+inter<-as.matrix(c(rep(inter.val,5),rep(0,p-5)))
 
 
+### number of violation space to consider
 Q = 5
 estimator.names <- c(paste("RF-q",0:(Q-1),sep=""),paste("Cor-q",0:(Q-1),sep=""))
-Coef.matrix.inter<-matrix(NA,nrow=nsim,ncol=2*Q)
-sd.matrix.inter<-matrix(NA,nrow=nsim,ncol=2*Q)
-colnames(Coef.matrix.inter) <- colnames(sd.matrix.inter) <- estimator.names
+### the fixed violation space for RF
+Coef.matrix.rf<-matrix(NA,nrow=nsim,ncol=2*Q)
+sd.matrix.rf<-matrix(NA,nrow=nsim,ncol=2*Q)
+colnames(Coef.matrix.rf) <- colnames(sd.matrix.rf) <- estimator.names
+### the fixed violation space for basis
+Coef.matrix.basis<-matrix(NA,nrow=nsim,ncol=Q)
+sd.matrix.basis<-matrix(NA,nrow=nsim,ncol=Q)
+colnames(Coef.matrix.basis) <- colnames(sd.matrix.basis) <- paste("Basis-q",0:(Q-1),sep="")
 
 
 # IV strength test
 iv.str <- iv.thol <- matrix(NA,nrow=nsim,ncol=Q)
 colnames(iv.str) <- colnames(iv.thol) <- paste("q",0:(Q-1),sep="")
-# signal strength test for data split estimator
-signal.str <- signal.thol <- matrix(NA,nrow=nsim,ncol=Q)
-colnames(signal.str) <- colnames(signal.thol) <- paste("q",0:(Q-1),sep="")
-# signal strength test for bias corrected estimator
-signal.str.cor <- signal.thol.cor <- matrix(NA,nrow=nsim,ncol=Q)
-colnames(signal.str.cor) <- colnames(signal.thol.cor) <- paste("q",0:(Q-1),sep="")
 
 
 # variance of epsilon
@@ -88,31 +80,38 @@ SigmaYD <- matrix(NA, nsim, Q)
 colnames(SigmaYD) <- estimator.names[1:Q]
 
 
-# the numerator of variance estimate, saved to speed up computation
-DT.Sq <- matrix(NA, nsim, Q)
-colnames(DT.Sq) <- paste("q",0:(Q-1),sep = "")
-# trace of TRF.V and (TRF.V)^2
-trace.T2 <- trace.T <- matrix(NA,nsim,Q)
-colnames(trace.T) <- colnames(trace.T2) <- paste("q",0:(Q-1),sep = "")
-
-
 ### selection
 Q.max  <- matrix(NA,nsim,2)
 colnames(Q.max) <- c("Basis","RF")
-qhat.c <- qhat.r <- validity <- SigmaSqY.Qmax <- Q.max
+q.comp <- q.robust <- SigmaSqY.Qmax <- Q.max
 # H <- matrix(NA,nsim,Q-1)
 # C.alpha <- matrix(NA,nsim,Q-1)
 Coef.robust <- matrix(NA,nsim,6)
 sd.robust <- matrix(NA,nsim,6)
-colnames(Coef.robust) <- colnames(sd.robust) <- 
+colnames(Coef.robust) <- colnames(sd.robust) <-
   c("Basis-comp","Basis-robust","RF-comp","RF-Cor-comp","RF-robust","RF-Cor-robust")
 
 ### oracle estimator
-oracle.est <- oracle.sd <- matrix(NA,nsim,3)
-colnames(oracle.est) <- colnames(oracle.sd) <- c("Basis","RF","RF-Cor")
+Coef.oracle <- sd.oracle <- matrix(NA,nsim,3)
+colnames(Coef.oracle) <- colnames(sd.oracle) <- c("Basis","RF","RF-Cor")
 
 
-for(i in 1:nsim){
+### naive/plain estimators
+Coef.naive <- sd.naive <- matrix(NA,nsim,2)
+colnames(Coef.naive) <- colnames(sd.naive) <- c("naiveRF","RF-full")
+
+
+### TSLS estimator
+Coef.TSLS <- sd.TSLS <- matrix(NA,nsim,3)
+colnames(Coef.TSLS) <- colnames(sd.TSLS) <- c("TSLS","TSLS-Inter","TSLS-Inter-NoAdj")
+
+
+### weak IV problem
+run.OLS <- weak.iv <- matrix(NA,nsim,2)
+colnames(run.OLS) <- colnames(weak.iv) <- c("Basis","RF")
+
+
+for(i in 1:nsim) {
   print(i)
   #### generate the data
   mu.error<-rep(0,2)
@@ -124,73 +123,73 @@ for(i in 1:nsim){
   X<-W[,-1]
   ###### generate the data for the treatment variable D
   if(f.index==1){
-    D=f_1(Z)+X%*%alpha+Error[,1]
-  }
-  if(f.index==2){
-    D=f_2(Z)+X%*%alpha+Error[,1]
-  }
-  if(f.index==3){
-    D=f_3(Z)+X%*%alpha+Error[,1]
-  }
-  if(f.index==4){
     D=f_1(Z)+X%*%alpha+Z*X%*%inter+Error[,1]
   }
-  if(f.index==5){
+  if(f.index==2){
     D=f_2(Z)+X%*%alpha+Z*X%*%inter+Error[,1]
   }
-  if(f.index==6){
-    D=f_3(Z)+X%*%alpha+Z*X%*%inter+Error[,1]
-  }
-  if(f.index==7){
-    D=f_4(Z)+X%*%alpha+Error[,1]
-  }
   ####### generate the outcome variable
-  if(vio.index==0){
-    Y=D*beta+ X%*% gamma+Error[,2]
-  }
+  # if(vio.index==0){
+  #   Y=D*beta+ X%*% gamma+Error[,2]
+  # }
   if(vio.index==1){
     Y=D*beta+ tau*Z+ X%*% gamma+Error[,2]
   }
   if(vio.index==2){
     Y=D*beta+ tau*(Z^2+Z-1)+ X%*% gamma+Error[,2] # difficult if no Z
   }
-  if(vio.index==3){
-    Y=D*beta+ tau*Z^3+ X%*% gamma+Error[,2]
-  }
-  if(vio.index==4){
-    Y=D*beta+ tau*Z+ Z^5/40+ X%*% gamma+Error[,2]
-  }
+  # if(vio.index==3){
+  #   Y=D*beta+ tau*Z^3+ X%*% gamma+Error[,2]
+  # }
+  # if(vio.index==4){
+  #   Y=D*beta+ tau*Z+ Z^5/40+ X%*% gamma+Error[,2]
+  # }
   #if(vio.index==5){
   #  Y=D*beta+ tau*(Z^2-1)+ 1/20*sin(Z/2)+ X%*% gamma+Error[,2]
   #}
-  
-  
-  ### Basis Approach
-  # basis.fit <- TSCI.basis.fit(W,D)
-  # 
-  # D.rep.basis <- basis.fit$D.rep
-  # M <- basis.fit$M
-  # knot <- basis.fit$knot
-  # 
-  # outputs.basis <- TSCI.basis.selection(Y, D, W, D.rep.basis, knot, M, Q=Q)
-  # Coef.robust[i,1:2] <- outputs.basis$Coef.vec
-  # sd.robust[i,1:2] <- outputs.basis$sd.vec
-  # SigmaSqY.Qmax[i,1] <- outputs.basis$SigmaSqY.Qmax
-  # Q.max[i,1] <- outputs.basis$Q.max; qhat.c[i,1] <- outputs.basis$q.comp; qhat.r[i,1] <- outputs.basis$q.robust;
-  # validity[i,1] <- outputs.basis$validity
-  # oracle.est[i,1] <- outputs.basis$prop.vec.all[vio.index+1]
-  # oracle.sd[i,1] <- outputs.basis$se.vec[vio.index+1]
-  
-  
+
+
+  ### TSLS Estimators
+  TSLS <- ivreg(Y~D+X|Z+X)
+  TSLS.Inter <- ivreg(Y~D+Z+X|Z+X+Z*X)
+  TSLS.Inter.noadj <- ivreg(Y~D+X|Z+X+Z*X)
+
+  Coef.TSLS[i,1] <- coef(TSLS)[2]
+  Coef.TSLS[i,2] <- coef(TSLS.Inter)[2]
+  Coef.TSLS[i,3] <- coef(TSLS.Inter.noadj)[2]
+  sd.TSLS[i,1] <- summary(TSLS)$coefficients[2,2]
+  sd.TSLS[i,2] <- summary(TSLS.Inter)$coefficients[2,2]
+  sd.TSLS[i,3] <- summary(TSLS.Inter.noadj)$coefficients[2,2]
+
+
+  ## Basis Approach
+  basis.fit <- TSCI.basis.fit(W,D)
+
+  D.rep.basis <- basis.fit$D.rep
+  M <- basis.fit$M
+  knot <- basis.fit$knot
+
+  outputs.basis <- TSCI.basis.selection(Y, D, W, D.rep.basis, knot, M, Q=Q)
+  if (length(outputs.basis$Coef.vec)>=Q) {
+    Coef.matrix.basis[i,] <- outputs.basis$Coef.vec
+    sd.matrix.basis[i,] <- outputs.basis$sd.vec
+  } else {
+    Coef.matrix.basis[i,] <- c(outputs.basis$Coef.vec, rep(NA,Q-length(outputs.basis$Coef.vec)))
+    sd.matrix.basis[i,] <- c(outputs.basis$sd.vec, rep(NA,Q-length(outputs.basis$sd.vec)))
+  }
+  Coef.robust[i,1:2] <- outputs.basis$Coef.robust
+  sd.robust[i,1:2] <- outputs.basis$sd.robust
+  SigmaSqY.Qmax[i,1] <- outputs.basis$SigmaSqY.Qmax
+  Q.max[i,1] <- outputs.basis$Q.max; q.comp[i,1] <- outputs.basis$q.comp; q.robust[i,1] <- outputs.basis$q.robust;
+  Coef.oracle[i,1] <- outputs.basis$Coef.vec[vio.index+1]
+  sd.oracle[i,1] <- outputs.basis$sd.vec[vio.index+1]
+  run.OLS[i,1] <- outputs.basis$run.OLS; weak.iv[i,1] <- outputs.basis$weak.iv
+
+
   ### random forest based methods
   forest.cov<-cbind(Z,W[,-1])
-  ### set mtry according to p to save time
-  # mtry from 1 to p if p<10, from p/3 to 2*p/3 if p>10
-  if (p+1<=11) {
-    mtry = 1:(p+1)
-  } else {
-    mtry = seq(round((p+1)/3), round(2*(p+1)/3), by=1)
-  }
+  # mtry from p/3 to 2*p/3
+  mtry = seq(round((p+1)/3), round(2*(p+1)/3), by=1)
   ### set max.depth and min.node.size for tuning
   ### larger max.depth and smaller min.node.size means more complex trees
   max.depth <- 0; min.node.size <- c(5,10,20)
@@ -198,10 +197,11 @@ for(i in 1:nsim){
 
   ### Data splitting random forest
   # use 2 to denote 2split
-  forest.2 <- TSRF.fit(forest.cov,D,mtry=mtry,max.depth=max.depth,min.node.size=min.node.size)
+  forest.2 <- TSRF.fit(forest.cov,D,mtry=mtry,max.depth=max.depth,min.node.size=min.node.size,split.prop=2/3)
   A1.ind <- forest.2$A1.ind
   # weight matrix
   weight.2 <- TSRF.weight(forest.2$nodes.A1)
+  rm(forest.2)
 
   Cov.aug <- W[,-1]
   for (q in 1:(Q-1)) {
@@ -210,55 +210,76 @@ for(i in 1:nsim){
 
 
   ### selection
-  outputs <- TSRF.Selection(Y, D, Cov.aug, A1.ind, weight.2, Q=Q)
+  outputs.2 <- TSRF.Selection(Y, D, Cov.aug, A1.ind, weight.2, Q=Q)
   ### outputs
-  Coef.matrix.inter[i,] <- outputs$Coef.vec
-  sd.matrix.inter[i,] <- outputs$sd.vec
-  SigmaSqY[i,] <- outputs$SigmaSqY
-  SigmaSqD[i] <- outputs$SigmaSqD
-  SigmaYD[i,] <- outputs$SigmaYD
-  iv.str[i,] <- outputs$iv.str; iv.thol[i,] <- outputs$iv.thol; DT.Sq[i,] <- outputs$DT.Sq
-  signal.str[i,] <- outputs$signal.str; signal.thol[i,] <- outputs$signal.thol;
-  signal.str.cor[i,] <- outputs$signal.str.cor; signal.thol.cor[i,] <- outputs$signal.thol.cor;
-  trace.T[i,] <- outputs$trace.T;   trace.T2[i,] <- outputs$trace.T2
+  Coef.matrix.rf[i,] <- outputs.2$Coef.vec
+  sd.matrix.rf[i,] <- outputs.2$sd.vec
+  SigmaSqY[i,] <- outputs.2$SigmaSqY
+  SigmaSqD[i] <- outputs.2$SigmaSqD
+  SigmaYD[i,] <- outputs.2$SigmaYD
+  iv.str[i,] <- outputs.2$iv.str; iv.thol[i,] <- outputs.2$iv.thol;
 
-  Coef.robust[i,3:6] <- outputs$Coef.robust; sd.robust[i,3:6] <- outputs$sd.robust;
-  # H[i,] <- outputs$H; C.alpha[i,] <- outputs$C.alpha;
-  SigmaSqY.Qmax[i,2] <- outputs$SigmaSqY.Qmax
-  Q.max[i,2] <- outputs$Q.max; qhat.c[i,2] <- outputs$qhat.c; qhat.r[i,2] <- outputs$qhat.r;
-  validity[i,2] <- outputs$validity
-  oracle.est[i,2] <- Coef.matrix.inter[i,vio.index+1]
-  oracle.est[i,3] <- Coef.matrix.inter[i,vio.index+Q+1]
-  oracle.sd[i,2] <- sd.matrix.inter[i,vio.index+1]
-  oracle.sd[i,3] <- sd.matrix.inter[i,vio.index+Q+1]
-  H[[i]] <- outputs$H
-  beta.diff[[i]] <- outputs$beta.diff
-  z.alpha[i] <- outputs$z.alpha
-  
+  Coef.robust[i,3:6] <- outputs.2$Coef.robust; sd.robust[i,3:6] <- outputs.2$sd.robust;
+  SigmaSqY.Qmax[i,2] <- outputs.2$SigmaSqY.Qmax
+  Q.max[i,2] <- outputs.2$Q.max; q.comp[i,2] <- outputs.2$q.comp; q.robust[i,2] <- outputs.2$q.robust;
+  Coef.oracle[i,2] <- Coef.matrix.rf[i,vio.index+1]
+  Coef.oracle[i,3] <- Coef.matrix.rf[i,vio.index+Q+1]
+  sd.oracle[i,2] <- sd.matrix.rf[i,vio.index+1]
+  sd.oracle[i,3] <- sd.matrix.rf[i,vio.index+Q+1]
+  run.OLS[i,2] <- outputs.2$run.OLS; weak.iv[i,2] <- outputs.2$weak.iv
+
+
+  ## random forest using plug-in method with data in A1
+  naiveRF.outputs <- naiveRF.stat(Y, D, Cov.aug[,-(1:(Q-1-vio.index))], A1.ind, weight.2)
+  Coef.naive[i,1] <- naiveRF.outputs$betaHat
+  sd.naive[i,1] <- naiveRF.outputs$sd
+  rm(weight.2)
+
+
+  ### random forest with full data
+  forest.full <- TSRF.full(Cov.aug[,-(1:(Q-1-vio.index))],D,mtry=mtry,max.depth=max.depth,min.node.size=min.node.size)
+  weight.full <- TSRF.weight(forest.full$nodes)
+  full.outputs <- TSRF.stat.full(Y,D,forest.cov,weight.full)
+  Coef.naive[i,2] <- full.outputs$betaHat
+  sd.naive[i,2] <- full.outputs$sd
+  rm(weight.full)
+  rm(forest.full)
+
 }
 
 
-apply(Coef.matrix.inter,2,mean)
-# apply(Coef.matrix.inter,2,sd)
-apply(sd.matrix.inter,2,mean)
-
-
+apply(Coef.matrix.rf,2,mean)
+apply(sd.matrix.rf,2,mean)
 ### coverage of fixed space for ramdom forest
-Cov.mat <- matrix(NA,nsim,ncol(sd.matrix.inter))
-colnames(Cov.mat) <- colnames(sd.matrix.inter)
-for (j in 1:ncol(sd.matrix.inter)) {
-  Cov.mat[,j] <- ifelse(Coef.matrix.inter[,j]-1.96*sd.matrix.inter[,j]<=beta &
-                          beta<=Coef.matrix.inter[,j]+1.96*sd.matrix.inter[,j],1,0)
+Cov.mat.rf <- matrix(NA,nsim,ncol(sd.matrix.rf))
+colnames(Cov.mat.rf) <- colnames(sd.matrix.rf)
+for (j in 1:ncol(sd.matrix.rf)) {
+  Cov.mat.rf[,j] <- ifelse(Coef.matrix.rf[,j]-1.96*sd.matrix.rf[,j]<=beta &
+                          beta<=Coef.matrix.rf[,j]+1.96*sd.matrix.rf[,j],1,0)
 }
-apply(Cov.mat,2,mean)
+apply(Cov.mat.rf,2,mean)
 
 
-### coverage of oracle method
-Cov.oracle <- matrix(NA,nsim,ncol(oracle.sd))
-colnames(Cov.oracle) <- colnames(oracle.sd)
-for (j in 1:ncol(oracle.sd)) {
-  Cov.oracle[,j] <- ifelse(oracle.est[,j]-1.96*oracle.sd[,j]<=beta &
-                          beta<=oracle.est[,j]+1.96*oracle.sd[,j],1,0)
+Coef.matrix.basis <- na.omit(Coef.matrix.basis)
+sd.matrix.basis <- na.omit(sd.matrix.basis)
+apply(Coef.matrix.basis,2,mean)
+apply(sd.matrix.basis,2,mean)
+### coverage of fixed space for basis
+Cov.mat.basis <- matrix(NA,nrow(sd.matrix.basis),ncol(sd.matrix.basis))
+colnames(Cov.mat.basis) <- colnames(sd.matrix.basis)
+for (j in 1:ncol(sd.matrix.basis)) {
+  Cov.mat.basis[,j] <- ifelse(Coef.matrix.basis[,j]-1.96*sd.matrix.basis[,j]<=beta &
+                          beta<=Coef.matrix.basis[,j]+1.96*sd.matrix.basis[,j],1,0)
+}
+apply(Cov.mat.basis,2,mean)
+
+
+### coverage of oracle violation space
+Cov.oracle <- matrix(NA,nsim,ncol(sd.oracle))
+colnames(Cov.oracle) <- colnames(sd.oracle)
+for (j in 1:ncol(sd.oracle)) {
+  Cov.oracle[,j] <- ifelse(Coef.oracle[,j]-1.96*sd.oracle[,j]<=beta &
+                          beta<=Coef.oracle[,j]+1.96*sd.oracle[,j],1,0)
 }
 apply(Cov.oracle,2,mean)
 
@@ -276,6 +297,26 @@ for (j in 1:ncol(sd.robust)) {
 apply(Cov.robust,2,mean)
 
 
-filename <- paste("TSCI-ConIV-Setting",f.index,"-Violation",vio.index,"-Interaction",inter.val,"-p",p,"-n",n,".RData",sep="")
+## coverage of naive methods
+Cov.naive <- matrix(NA,nsim,ncol(sd.naive))
+colnames(Cov.naive) <- colnames(sd.naive)
+for (j in 1:ncol(sd.naive)) {
+  Cov.naive[,j] <- ifelse(Coef.naive[,j]-1.96*sd.naive[,j]<=beta &
+                             beta<=Coef.naive[,j]+1.96*sd.naive[,j],1,0)
+}
+apply(Cov.naive,2,mean)
+
+
+### coverage of TSLS
+Cov.TSLS <- matrix(NA,nsim,ncol(sd.TSLS))
+colnames(Cov.TSLS) <- colnames(sd.TSLS)
+for (j in 1:ncol(sd.TSLS)) {
+  Cov.TSLS[,j] <- ifelse(Coef.TSLS[,j]-1.96*sd.TSLS[,j]<=beta &
+                           beta<=Coef.TSLS[,j]+1.96*sd.TSLS[,j],1,0)
+}
+apply(Cov.TSLS,2,mean)
+
+
+filename <- paste("TSCI-ConIV-Setting",f.index,"-Interaction",inter.val,"-Violation",vio.index,"-p",p,"-n",n,".RData",sep="")
 save.image(filename)
 
